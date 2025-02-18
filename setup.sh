d139bfaa5427b3993c18063a7156f2a7b297e2c8
fkabanga7 <195691665+fkabanga7@users.noreply.github.com>
2025-02-18 14:15:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # Install dependencies listed in requirements.txt
+pip install -r #!/bin/bash
+
+# Install dependencies listed in requirements.txt
 pip install -r requirements.txt
 
 # Download spaCy model
 python -m spacy download en_core_web_sm
+brew install python@3.10
+python3.10 --version
+pip install --upgrade pip
+pip install --upgrade setuptools
+
 
