0ae4b93ac19165952dcb8fb54a79c8c4bcd0d6c9
fkabanga7 <195691665+fkabanga7@users.noreply.github.com>
2025-02-21 22:39:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,21 +3,15 @@ pip install --upgrade pip
 pip install --upgrade setuptools
 pip install --upgrade numpy
 pip install --upgrade pip setuptools wheel
+pip install --upgrade numpy scikit-learn pandas streamlit
+pip install --upgrade --force-reinstall scikit-learn
 # Install dependencies listed in requirements.txt
 pip install -r requirements.txt
-pip install --upgrade --force-reinstall scikit-learn
-# Ensure all dependencies are correct
-pip install --upgrade numpy scikit-learn pandas streamlit
-# Install the correct version of python if needed (but confirm the environment version first)
-#brew install python@3.10  # You can skip this if your environment already has the correct version of Python
-# setup.sh
-apt-get update
-apt-get install -y python3-distutils
 
-# Download spaCy model
-python -m spacy download en_core_web_sm
 
-# Ensure correct version of pip and setuptools
+
+
+
 
 
 
