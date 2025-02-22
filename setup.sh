d485d67dcd16327618e1541296089e50dedef352
fkabanga7 <195691665+fkabanga7@users.noreply.github.com>
2025-02-21 22:44:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install --upgrade numpy
-pip install --upgrade pip setuptools wheel
+
 pip install --upgrade numpy scikit-learn pandas streamlit
 pip install --upgrade --force-reinstall scikit-learn
 # Install dependencies listed in requirements.txt
